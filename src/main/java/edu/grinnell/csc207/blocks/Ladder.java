b7e0765da3bc8a96f800ee01e54d498ce1862d33
Jake Bell <3up3down10@gmail.com>
2024-09-25 20:48:05 -0500

    added ladder

---
+++ src/main/java/edu/grinnell/csc207/blocks/Ladder.java
@@ -0,0 +1,120 @@
+package edu.grinnell.csc207.blocks;
+
+public class Ladder implements AsciiBlock {
+    // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The element in which the steps will be made of.
+   */
+  AsciiBlock stepElement;
+
+  /**
+   * The number of times the step will be repeated vertically.
+   */
+  int stepRepetitions;
+
+  /**
+   * What the sides of the ladder will look like.
+   */
+  char sideRail;
+
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new grid with the specified arrangement.
+   *
+   * @param stepElement
+   *   What the ladder steps will be made of.
+   * @param stepRepitions
+   *   The number of vertical repetitions in the grid.
+   * @param sideRail
+   *   The character of the side rail of the ladder.
+   */
+  public Ladder(AsciiBlock stepElement, int stepRepetitions,
+      char sideRail) {
+    this.stepElement = stepElement;
+    this.stepRepetitions = stepRepetitions;
+    this.sideRail = sideRail;
+  } // Ladder(AsciiBlock, int, char)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Get one row from the block.
+   *
+   * @param i the number of the row
+   *
+   * @return row i.
+   *
+   * @exception Exception
+   *   If the row is invalid.
+   */
+  public String row(int i) throws Exception {
+    String madeline = "";
+    String sides = Character.toString(this.sideRail);
+
+    if ((i < 0) || (i >= this.height())) {
+      // Outside of normal bounds
+      throw new Exception("Invalid row " + i);
+    } // if
+
+    if (i % 2 == 0) {
+      madeline += sides + this.stepElement + sides;
+    } else {
+      madeline += sides + this.stepElement.width() + sides;
+    }
+
+    return madeline;
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   *
+   * @return the number of rows
+   */
+  public int height() {
+   return ((this.stepRepetitions - 1) * 2) + 1; 
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   *
+   * @return the number of columns
+   */
+  public int width() {
+    return this.stepElement.width() + 2;   // STUB
+  } // width()
+
+  /**
+   * Determine if another block is structurally equivalent to this block.
+   *
+   * @param other
+   *   The block to compare to this block.
+   *
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
+   */
+  public boolean eqv(AsciiBlock other) {
+    return ((other instanceof Ladder) && (this.eqv((Ladder) other)));
+  } // eqv(AsciiBlock)
+
+  /**
+   * Determine if another grid is structurally equivalent to this grid.
+   *
+   * @param other
+   *   The grid to compare to this grid.
+   *
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
+   */
+  public boolean eqv(Ladder other) {
+    return (this.sideRail == other.sideRail) && (this.stepRepetitions == other.stepRepetitions)
+        && (this.stepElement.eqv(other.stepElement));
+  } // eqv(Grid)
+}
