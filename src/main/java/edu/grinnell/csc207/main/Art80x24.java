f1b4ed7231e3b2a1cf760b44818250a44bb73a04
1maral <erdenemaral>
2024-09-26 19:23:44 -0500

    Art has been finished - PLEASE add more doc

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -1,17 +1,17 @@
 package edu.grinnell.csc207.main;
 
+import java.io.PrintWriter;
+
 import edu.grinnell.csc207.blocks.AsciiBlock;
 import edu.grinnell.csc207.blocks.Boxed;
 import edu.grinnell.csc207.blocks.Grid;
 import edu.grinnell.csc207.blocks.HAlignment;
 import edu.grinnell.csc207.blocks.HComp;
+import edu.grinnell.csc207.blocks.HFlip;
 import edu.grinnell.csc207.blocks.Line;
-import edu.grinnell.csc207.blocks.Lines;
 import edu.grinnell.csc207.blocks.Rect;
 import edu.grinnell.csc207.blocks.VAlignment;
 import edu.grinnell.csc207.blocks.VComp;
-
-import java.io.PrintWriter;
 
 /**
  * Create and print an amazing 80x24 ASCII artwork.
@@ -31,17 +31,48 @@ public class Art80x24 {
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
+
     AsciiBlock example = new Grid(new Line("."),80,24);
-    AsciiBlock pattern1 = new Rect('#', 10, 3);
-    AsciiBlock pattern2 = new Boxed(new Rect(' ', 8, 3));
-    AsciiBlock hair1 = new Grid(pattern1,8,1);
-    AsciiBlock forehead1 = new Grid(pattern2,6,1);
-    AsciiBlock hair2 = new HComp(VAlignment.CENTER, new AsciiBlock[] {pattern1, forehead1, pattern1});
-    AsciiBlock forehead2 = new Grid(pattern2,8,1);
-    AsciiBlock foreheadFull = new VComp(HAlignment.CENTER, new AsciiBlock[] {hair1, hair2, forehead2});
-    AsciiBlock art = new Grid(new Line("."), 80, 24);
     AsciiBlock.print(pen, example);
-    AsciiBlock.print(pen, foreheadFull);
+
+    // Creating the essential blocks to use
+    AsciiBlock black = new Rect('#', 10, 3);
+    AsciiBlock skin = new Rect(' ', 10, 3);
+    AsciiBlock white = new Boxed(new Rect(' ', 8, 1));
+    AsciiBlock nose = new Rect('_', 10, 3);
+
+    // Creating the hair + forehead of the character
+    // First Layer
+    AsciiBlock hair1 = new Grid(black,8,1);
+    // Second Layer
+    AsciiBlock hair2 = new HComp(VAlignment.CENTER, new AsciiBlock[] {black, new Grid(skin,6,1), black});
+    // Third layer
+    AsciiBlock forehead = new Grid(skin,8,1);
+    AsciiBlock foreheadFull = new VComp(HAlignment.CENTER, new AsciiBlock[] {hair1, hair2, forehead});
+
+    // Creating the eye layer of the character
+    AsciiBlock eye1 = new HComp(VAlignment.CENTER, new AsciiBlock[] {skin, white, black, skin});
+    // Flipping the first eye to create second eye
+    AsciiBlock eye2 = new HFlip(eye1);
+    AsciiBlock eyeFull = new HComp(VAlignment.CENTER, new AsciiBlock[] {eye1, eye2});
+
+    // Creating the nose layer of the character
+    AsciiBlock nose1 = new HComp(VAlignment.CENTER, new AsciiBlock[] {new Grid(skin,3,1), nose});
+    // Flipping the first nose to create second nose
+    AsciiBlock nose2 = new HFlip(nose1);
+    AsciiBlock noseFull = new HComp(VAlignment.CENTER, new AsciiBlock[] {nose1, nose2});
+
+    // Creating the mouth layer of the character
+    AsciiBlock mouth1 = new HComp(VAlignment.CENTER, new AsciiBlock[] {new Grid(skin,2,1), black, skin});
+    // Flipping the first nose to create second nose
+    AsciiBlock mouth2 = new HFlip(mouth1);
+    AsciiBlock mouth3 = new HComp(VAlignment.CENTER, new AsciiBlock[] {skin, skin, new Grid(black,4,1), skin, skin});
+    AsciiBlock mouth4 = new HComp(VAlignment.CENTER, new AsciiBlock[] {mouth1, mouth2});
+    AsciiBlock mouthFull = new VComp(HAlignment.CENTER, new AsciiBlock[] {mouth4, mouth3});
+
+    AsciiBlock art = new VComp(HAlignment.CENTER, new AsciiBlock[] {foreheadFull, eyeFull, noseFull, mouthFull, forehead});
+
+    AsciiBlock.print(pen, art);
     pen.close();
   } // main(String[])
 } // class Art80x24
